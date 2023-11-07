2a6b71940a6f3d0393a571188c8ed998b789fc5b
Zainab Tasleem Wani <84972587+zainabwani@users.noreply.github.com>
2023-11-07 19:36:31 +0530

    Create helloworld.c

---
+++ helloworld.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Print "Hello, World!"
+    printf("Hello, World! This is my first Repository\n");
+
+    // Display information about yourself
+    printf("I'm Zainab, and I'm absolutely thrilled to embark on my journey with GitHub. As someone deeply passionate about the world of information technology, I recently completed my postgraduate studies in IT, immersing myself in the ever-evolving landscape of technology, problem-solving, and innovation.");
+
+    return 0;
+}
